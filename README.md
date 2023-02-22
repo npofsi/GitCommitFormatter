fa05463f179fbf169756a3340be4256c72f3a21e
npofsi | N/P硅 <npofsi@outlook.com>
2023-02-22 11:57:11 +0800

    Feat: Update

--- README.md
+++ README.md
@@ -12,14 +12,14 @@ Use `./committer.sh` instead `git commit -m "message"`
 
 You can choose commit type:
 
-	    (0) feat:新功能（feature）"
-	    (1) fix :修补bug"
-	    (2) docs:文档相关更新（documentation）"
-	    (3) style:代码格式变化（不影响运行）"
-	    (4) refactor:重构（既不新增也不修复bug）"
-	    (5) perf:提高性能"
-	    (6) test:测试相关更新"
-	    (7) chore:构建过程或辅助工具的变动"
-	    (8) cleanup:不影响代码逻辑的提交"
-	    (9) tracking:跟踪相关提交"
+	    (0) Feat:新功能（feature）"
+	    (1) Fix :修补bug"
+	    (2) Docs:文档相关更新（documentation）"
+	    (3) Style:代码格式变化（不影响运行）"
+	    (4) Refactor:重构（既不新增也不修复bug）"
+	    (5) Perf:提高性能"
+	    (6) Test:测试相关更新"
+	    (7) Chore:构建过程或辅助工具的变动"
+	    (8) Cleanup:不影响代码逻辑的提交"
+	    (9) Tracking:跟踪相关提交"
 
--- committer.sh
+++ committer.sh
@@ -9,25 +9,25 @@ git add .
 echo ""
 echo "  欢迎使用gitCommit格式化工具"
 echo ""
-echo "    (0) feat:新功能（feature）"
-echo "    (1) fix :修补bug"
-echo "    (2) docs:文档相关更新（documentation）"
-echo "    (3) style:代码格式变化（不影响运行）"
-echo "    (4) refactor:重构（既不新增也不修复bug）"
-echo "    (5) perf:提高性能"
-echo "    (6) test:测试相关更新"
-echo "    (7) chore:构建过程或辅助工具的变动"
-echo "    (8) cleanup:不影响代码逻辑的提交"
-echo "    (9) tracking:跟踪相关提交"
+echo "    (0) Feat:新功能（feature）"
+echo "    (1) Fix :修补bug"
+echo "    (2) Docs:文档相关更新（documentation）"
+echo "    (3) Style:代码格式变化（不影响运行）"
+echo "    (4) Refactor:重构（既不新增也不修复bug）"
+echo "    (5) Perf:提高性能"
+echo "    (6) Test:测试相关更新"
+echo "    (7) Chore:构建过程或辅助工具的变动"
+echo "    (8) Cleanup:不影响代码逻辑的提交"
+echo "    (9) Tracking:跟踪相关提交"
 echo ""
 
-typeArray=("feat" "fix" "docs" "style" "refactor" "perf" "test" "chore" "cleanup" "tracking")
+typeArray=("Feat" "Fix" "Docs" "Style" "Refactor" "Perf" "Test" "Chore" "Cleanup" "Tracking")
 
 inputType(){
 
 	stype=0;
 
-	read -p "  选择commit类型 (0):" stype
+	read -p "  选择 Commit 类型 (0):" stype
 
 	if [[ $stype =~ ^-?[0-9]+$ && $stype -lt ${#typeArray[@]} ]] #&& $stype -lt ${#typeArray}
 	then
@@ -38,7 +38,7 @@ inputType(){
 		typee="${typeArray[0]}"
 		message="${message}${typeArray[0]}"
 	else
-	 	echo "  [Error]参数错误"
+	 	echo "  [Error] 参数错误"
 	 	inputType
 	fi
 }
@@ -50,7 +50,7 @@ inputScope(){
 
 	scopee=""
 
-	read -p "  输入这次commit的影响范围(空):" scopee
+	read -p "  输入这次 Commit 的影响范围 (空):" scopee
 
 	if [[ ${#scopee} -gt 0 ]]
 	then
@@ -68,13 +68,13 @@ inputSubject(){
 
 	subject=""
 
-	read -p "  简单描述这次commit(空):" subject
+	read -p "  简单描述这次 Commit (空):" subject
 
 	if [[ ${#subject} -gt 0 && $(( ${#message} + ${#subject} )) -lt 100 ]]
 	then
 		message="${message} ${subject}"
 	else
-		echo "  [Error]没有描述或描述过长"
+		echo "  [Error] 没有描述或描述过长"
 		inputSubject
 	fi
 }
@@ -86,7 +86,7 @@ inputBody(){
 
 	body=""
 
-	read -p "  输入commit正文(空):" body
+	read -p "  输入 Commit 正文 (空):" body
 
 	if [[ ${#body} -gt 0 ]]
 	then
